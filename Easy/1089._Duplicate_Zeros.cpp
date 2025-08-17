7e0cb49db596a35fd0bfb5e42f0f52338d2c0e01
JayThakare05 <thakarejay2005@gmail.com>
2025-08-17 17:41:50 +0530

    Day 7

---
+++ Easy/1089._Duplicate_Zeros.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    void duplicateZeros(vector<int>& arr) {
+        int n=arr.size();
+        for(int i=0;i<arr.size();i++){
+            if(arr[i]==0){
+                arr.insert(arr.begin()+i+1,0);
+                i+=1;
+            }
+        }
+        arr.resize(n);
+        
+    }
+};
