e15d2cac87596ccea3a3ea8b24e45cbe41fe6aa1
JayThakare05 <thakarejay2005@gmail.com>
2025-08-19 00:47:37 +0530

    Day 9

---
+++ Easy/67._Add_Binary.cpp
@@ -0,0 +1,38 @@
+// Given two binary strings a and b, return their sum as a binary string.
+
+ 
+
+// Example 1:
+
+// Input: a = "11", b = "1"
+// Output: "100"
+// Example 2:
+
+// Input: a = "1010", b = "1011"
+// Output: "10101"
+ 
+
+// Constraints:
+
+// 1 <= a.length, b.length <= 104
+// a and b consist only of '0' or '1' characters.
+// Each string does not contain leading zeros except for the zero itself.
+class Solution {
+public:
+    
+    string addBinary(string a, string b) {
+        string sol="";
+        int carry=0;
+        int i=a.length()-1;
+        int j=b.length()-1;
+        while(i>=0||j>=0||carry){
+            int sum=carry;
+            if(i>=0) sum+=a[i--]-'0';
+            if(j>=0) sum+=b[j--]-'0';
+            sol +=(sum%2)+'0';
+            carry = sum/2;
+        }
+        reverse(sol.begin(),sol.end());
+        return sol;
+    }
+};
